d7270fda4fe2192d79cbe200a001bba90fcba62b
xjtushilei <xjtushilei@foxmail.com>
2017-06-12 15:21:42 +0800

    config-server

--- spring-cloud-config-server/src/main/java/com/xjtushilei/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/xjtushilei/SpringCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.xjtushilei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
     public static void main(String[] args) {
