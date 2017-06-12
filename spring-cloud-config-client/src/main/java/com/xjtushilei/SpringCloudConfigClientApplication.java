1639498fd10a1d4d955460a6d5ecc29f540d4dd2
xjtushilei <xjtushilei@foxmail.com>
2017-06-12 15:40:16 +0800

    config-config init

---
+++ spring-cloud-config-client/src/main/java/com/xjtushilei/SpringCloudConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.xjtushilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+	}
+}
